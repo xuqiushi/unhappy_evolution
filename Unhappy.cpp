518bd066cabeb4b5e553e76d5361b4ae09459859
qiushi.xu <qiushi.xu@17zuoye.com>
2021-03-23 11:22:59 +0800

    简化部分代码

--- Unhappy.cpp
+++ Unhappy.cpp
@@ -8,41 +8,44 @@ Unhappy::Unhappy(QWidget *parent)
       main_layout_(new QVBoxLayout()),
       head_bar_(new QFrame()),
       head_h_layout_(new QHBoxLayout()),
-      parse_json_button_(new QPushButton(QString("json解析"))),
-      file_encoding_transform_button_(new QPushButton(QString("文件UTF-8转GB18030"))),
-      preview_csv_button_(new QPushButton(QString("快速预览csv"))),
-      file_encoding_transform_(new file_encoding_transform::MainView(this)),
-      preview_csv_(new preview_csv::MainView(this)),
-      parse_json_(new parse_json::MainView(this)) {
+      button_map_(
+          std::map<GlobalFuncType, QPushButton *>{
+              {PARSE_JSON, new QPushButton(QString("json解析"))},
+              {FILE_ENCODING_TRANSFORM, new QPushButton(QString("文件UTF-8转GB18030"))},
+              {FAST_PREVIEW_CSV, new QPushButton(QString("快速预览csv"))}
+          }
+      ),
+      function_map_(
+          std::map<GlobalFuncType, QWidget *>{
+              {PARSE_JSON, new parse_json::MainView(this)},
+              {FILE_ENCODING_TRANSFORM, new file_encoding_transform::MainView(this)},
+              {FAST_PREVIEW_CSV, new preview_csv::MainView(this)}
+          }
+      ) {
     // 配置ui文件
     ui_->setupUi(this);
     // 创建第一行导航按钮
-    this->file_encoding_transform_button_->setFocusPolicy(Qt::NoFocus);
-    this->preview_csv_button_->setFocusPolicy(Qt::NoFocus);
-    this->parse_json_button_->setFocusPolicy(Qt::NoFocus);
     this->control_group_ = new QButtonGroup(this->head_h_layout_);
-    // 隐藏content部分
-    this->file_encoding_transform_->hide();
-    this->preview_csv_->hide();
-    this->parse_json_->hide();
+    for (auto &button : this->button_map_) {
+        button.second->setFocusPolicy(Qt::NoFocus);
+        this->control_group_->addButton(button.second);
+        // 主布局上半部分为选项
+        this->head_h_layout_->addWidget(button.second);
+        // 主布局下半部分通过点击按钮进行变化
+        connect(button.second, &QPushButton::pressed, this, &Unhappy::changeActivePage);
+        // 添加按钮与key反向关联
+        this->reversed_button_map_.insert(
+            std::make_pair(button.second, button.first)
+        );
+        // 隐藏content部分
+        this->function_map_.find(button.first)->second->hide();
+    }
     // 显示默认激活的页面
-    this->active_page_ = this->parse_json_;
+    this->active_page_ = this->function_map_.find(PARSE_JSON)->second;
     this->active_page_->show();
-    // 配置主布局为垂直布局
-    // 主布局上半部分为选项
     this->head_bar_->setFrameStyle(QFrame::StyledPanel | QFrame::Plain);
     this->control_group_->setExclusive(true);
-    this->control_group_->addButton(this->file_encoding_transform_button_);
-    this->control_group_->addButton(this->preview_csv_button_);
-    this->control_group_->addButton(this->parse_json_button_);
-    this->head_h_layout_->addWidget(this->parse_json_button_);
-    this->head_h_layout_->addWidget(this->file_encoding_transform_button_);
-    this->head_h_layout_->addWidget(this->preview_csv_button_);
     this->head_bar_->setLayout(this->head_h_layout_);
-    // 主布局下半部分通过点击按钮进行变化
-    connect(this->file_encoding_transform_button_, &QPushButton::pressed, this, &Unhappy::changeActivePage);
-    connect(this->preview_csv_button_, &QPushButton::pressed, this, &Unhappy::changeActivePage);
-    connect(this->parse_json_button_, &QPushButton::pressed, this, &Unhappy::changeActivePage);
     // 分配主布局上下比例
     this->main_layout_->addWidget(this->head_bar_, 1);
     this->main_layout_->addWidget(this->active_page_, 9);
@@ -60,13 +63,7 @@ void Unhappy::changeActivePage() {
     tmp = this->active_page_;
     // 关闭现在这个widget
     tmp->hide();
-    if (button == this->file_encoding_transform_button_) {
-        this->active_page_ = this->file_encoding_transform_;
-    } else if (button == this->preview_csv_button_) {
-        this->active_page_ = this->preview_csv_;
-    } else if (button == this->parse_json_button_) {
-        this->active_page_ = this->parse_json_;
-    }
+    this->active_page_ = this->function_map_.find(this->reversed_button_map_.find(button)->second)->second;
     // 替换现在的widget为需要激活的widget
     this->active_page_->show();
     this->main_layout_->replaceWidget(tmp, this->active_page_);
--- Unhappy.h
+++ Unhappy.h
@@ -1,6 +1,8 @@
 #ifndef UNHAPPY_EVOLUTION__UNHAPPY_H_
 #define UNHAPPY_EVOLUTION__UNHAPPY_H_
 
+#include <map>
+#include <unordered_set>
 #include <QWidget>
 #include <QButtonGroup>
 #include <QPushButton>
@@ -13,6 +15,12 @@ QT_BEGIN_NAMESPACE
 namespace Ui { class Unhappy; }
 QT_END_NAMESPACE
 
+enum GlobalFuncType {
+    PARSE_JSON = 0x0,  // 解析json部分
+    FILE_ENCODING_TRANSFORM = 0x1,  // 文件转码部分
+    FAST_PREVIEW_CSV = 0x2  // csv预览部分
+};
+
 class Unhappy : public QWidget {
   Q_OBJECT
 
@@ -23,16 +31,13 @@ class Unhappy : public QWidget {
   private:
     Ui::Unhappy *ui_;
     QWidget *active_page_;
-    QPushButton *file_encoding_transform_button_;
-    QPushButton *preview_csv_button_;
-    QPushButton *parse_json_button_;
+    std::map<GlobalFuncType, QPushButton *> button_map_;
+    std::map<QObject *, GlobalFuncType> reversed_button_map_;
+    std::map<GlobalFuncType, QWidget *> function_map_;
     QVBoxLayout *main_layout_;  // 配置主布局为垂直布局
     QFrame *head_bar_;  // 上部主菜单
     QHBoxLayout *head_h_layout_;  // 上部菜单布局
     QButtonGroup *control_group_;  // 上部菜单按钮组
-    file_encoding_transform::MainView *file_encoding_transform_;  // 文件转码部分
-    preview_csv::MainView *preview_csv_;  // csv预览部分
-    parse_json::MainView *parse_json_;  // 解析json部分
 
 
   private slots:
